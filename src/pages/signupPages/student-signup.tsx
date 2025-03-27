9a1a716751dab55bece2eb62a24ab44fd6b3ff50
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-03-27 19:48:55 +0100

    update student-signup.tsx

--- src/pages/signupPages/student-signup.tsx
+++ src/pages/signupPages/student-signup.tsx
@@ -1,5 +1,5 @@
 import SignupForm from "@/components/signup-form";
-import Navbar from "@/components/ui/navbar";
+import Navbar from "@/components/ui/Navbar";
 
 export default function StudentSignup() {
   return (
